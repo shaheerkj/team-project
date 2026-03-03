9711584b11ff1ddf58f204c2f87678808c44bbcd
Syed Shaheer Khalid <shaheerkjaffer@gmail.com>
2026-03-03 05:56:58 +0500

    Implemented login feature

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("welcome to another github repo")
+import hashlib
+
+# Simulated database (in real apps, this would be a real DB)
+users_db = {
+    "shaheer": hashlib.sha256("mypassword123".encode()).hexdigest()
+}    
+def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+def login():           
+  username = input("Enter username: ")
+  password = input("Enter password: ")                  
+
+  if username in users_db:
+    hashed_input_password = hash_password(password)
+
+    if users_db[username] == hashed_input_password:                                                
+
+      print("✅ Login successful!")                                                        
+    else:         
+      print("❌ Incorrect password.")                                                                  
+  else:                                                                                
+    print("❌ User not found.")                                                                                
+login()
