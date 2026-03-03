3cf8dbe7c9128647b2f230fcf9f7c7bb924851e7
shaheerkj <shaheerkjaffer@gmail.com>
2026-03-03 10:44:57 +0500

    feat: updated py

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-# added a comment
-import hashlib
+# main.py
 
-# Simulated database (in real apps, this would be a real DB)
-users_db = {
-    "shaheer": hashlib.sha256("mypassword123".encode()).hexdigest()
-}    
-def hash_password(password):
-        return hashlib.sha256(password.encode()).hexdigest()
+def main():
+    print("Hello, CI Pipeline!")
 
-def login():           
-  username = input("Enter username: ")
-  password = input("Enter password: ")                  
-
-  if username in users_db:
-    hashed_input_password = hash_password(password)
-
-    if users_db[username] == hashed_input_password:                                                
-
-      print("✅ Login successful!")                                                        
-    else:         
-      print("❌ Incorrect password.")                                                                  
-  else:                                                                                
-    print("❌ User not found.")                                                                                
-login()
+if __name__ == "__main__":
+    main()
